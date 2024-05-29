c3ce74123f077d9c550b1ed7b2121f25335b5a23
Manu Arora <manuarorawork@gmail.com>
2024-05-29 12:15:07 +0530

    add: grid and dot background check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AvatarPage from "./avatar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-grid-slate-200">
       <AvatarPage />
     </main>
   );
